68e18633fd116ce3b356b862c5703d2a45a31efc
evaldoamericojr-cmd <evaldo.americo.jr@gmail.com>
2025-09-20 10:32:28 -0300

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,10 @@
-// pages/index.tsx
 export default function Home() {
   return (
-    <main style={{display: 'grid', placeItems: 'center', height: '100vh'}}>
-      <h1 style={{fontSize: '2rem', fontWeight: 800}}>
-        ✅ SatoshiJr site online!
+    <main style={{ display: 'grid', placeItems: 'center', height: '100vh' }}>
+      <h1 style={{ fontSize: '2rem', fontWeight: 'bold' }}>
+        🚀 Hello SatoshiJr!
       </h1>
-      <p>Next.js (Pages Router) rodando na Vercel.</p>
     </main>
   );
 }
+
